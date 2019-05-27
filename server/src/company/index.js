1e6e8105a5790f81229bb2884f3e11d03f593626
Conor O'Flanagan <oflanac52@gmail.com>
2019-05-27 23:37:50 +0100

    don't allow authing of emails that already exist

--- server/src/company/index.js
+++ server/src/company/index.js
@@ -1,13 +1,24 @@
 const routes = require("express").Router();
 
-const { addAuthorizedEmail, deleteAuthorizedEmail } = require("../services/db");
+const {
+  addAuthorizedEmail,
+  deleteAuthorizedEmail,
+  isUser
+} = require("../services/db");
 
-routes.post("/authorizeemail", (req, res) => {
+routes.post("/authorizeemail", async (req, res) => {
   const { email, companyId } = req.body;
+  const alreadyExistsAsUser = await isUser(email);
+  if (alreadyExistsAsUser) {
+    res.statusMessage = "Cannot use this email";
+    res.status(500).end();
+    return;
+  }
   addAuthorizedEmail(email, companyId).then(dbRes => {
     if (dbRes.error) {
       res.statusMessage = "Error authorizing email";
       res.status(500).end();
+      return;
     }
     res.json({ message: "Email authorized" });
   });
